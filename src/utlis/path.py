b5efb1c390d9f6c56ab75e75b3b7530c92ba9841
Junser Hwang <hzser123@gmail.com>
2023-08-23 23:16:50 +0900

    [FIX] path 에러 수정

--- src/utlis/path.py
+++ src/utlis/path.py
@@ -1,6 +1,7 @@
 import os
 
-current_working_directory = os.getcwd()
+# current_working_directory = os.getcwd()
+
 
 def get_absolute_path(path: str) -> str:
-    return os.path.join(current_working_directory, path)
+    return os.path.join('/app', path)
