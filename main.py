64e3d25cde4fc8fb918f8885bcacefa736f91c92
Junser Hwang <hzser123@gmail.com>
2023-09-02 00:23:23 +0900

    [FIX] config validation 이전 되어 메인함수에선 제거

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.model.model import init_db
 from src.utlis.config import load_config
 from src.utlis.log import setup_logging
 from src.core.mysql_client import run_mysqldump
-from src.utlis.validation import database_validation, config_file_validation
+from src.utlis.validation import database_validation
 
 
 def main():
@@ -16,11 +16,6 @@ def main():
 
     # /app/config/config.yaml 파일을 로드합니다.
     conf = load_config()
-
-    # Config에 입력된 내용 체크
-    logging.info("Start Validation config.yaml.....")
-    config_file_validation()
-    logging.info("config.yaml ok")
 
     # logging.info("Start Validation smtp.....")
     # smtp_validation()
